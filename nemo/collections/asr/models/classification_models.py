cce0bde051dd14f85441e06a1c4b153e096baf71
Ewald Enzinger <ewald.enzinger@entn.at>
2022-06-29 12:02:36 +0200

    Fix dataset creation with bucketing for audio classification models

--- nemo/collections/asr/models/classification_models.py
+++ nemo/collections/asr/models/classification_models.py
@@ -226,10 +226,11 @@ class _EncDecBaseModel(ASRModel, ExportableEncDecModel):
             shuffle_n = config.get('shuffle_n', 4 * config['batch_size']) if shuffle else 0
             dataset = audio_to_label_dataset.get_tarred_classification_label_dataset(
                 featurizer=featurizer,
-                config=OmegaConf.to_container(config),
+                config=config,
                 shuffle_n=shuffle_n,
                 global_rank=self.global_rank,
                 world_size=self.world_size,
+                augmentor=augmentor,
             )
             shuffle = False
             batch_size = config['batch_size']
@@ -243,13 +244,13 @@ class _EncDecBaseModel(ASRModel, ExportableEncDecModel):
             if 'vad_stream' in config and config['vad_stream']:
                 logging.info("Perform streaming frame-level VAD")
                 dataset = audio_to_label_dataset.get_speech_label_dataset(
-                    featurizer=featurizer, config=OmegaConf.to_container(config)
+                    featurizer=featurizer, config=config, augmentor=augmentor
                 )
                 batch_size = 1
                 collate_func = dataset.vad_frame_seq_collate_fn
             else:
                 dataset = audio_to_label_dataset.get_classification_label_dataset(
-                    featurizer=featurizer, config=OmegaConf.to_container(config)
+                    featurizer=featurizer, config=config, augmentor=augmentor
                 )
                 batch_size = config['batch_size']
                 collate_func = dataset.collate_fn
